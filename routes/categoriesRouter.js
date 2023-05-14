4163e5c557958940f4b348dca2378ad8dbc6f3e3
Juan Manuel Quijano <juanquijano1352@gmail.com>
2023-05-13 23:23:58 -0500

    Implementing status code in code

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -33,7 +33,7 @@ router.get('/:id',(req,res)=>{
 router.post('/',(req,res)=>{
   const body = req.body;
 
-  res.json({
+  res.status(201).json({
     message:'Created',
     body
   })
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -34,7 +34,7 @@ router.get('/:id',(req,res)=>{
 router.post('/',(req,res)=>{
   const body = req.body;
 
-  res.json({
+  res.status(201).json({
     message:'Created',
     body
   })
