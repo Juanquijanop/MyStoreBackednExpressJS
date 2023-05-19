ca56691a000854d4c1857bb3f6585690306e73bc
Juan Manuel Quijano <juanquijano1352@gmail.com>
2023-05-18 23:31:18 -0500

    Implementing Middlewares in our program

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const routerApi = require('./routes')
-
+const {logErrors, errorHandler} = require('./middlewares')
 app.use(express.json())
 routerApi(app)
+
+
+app.use(logErrors);
+app.use(errorHandler);
+
 
 app.listen(port)
